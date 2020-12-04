138168a92f9346e2067424b487f622d33729e5cb
Daniil <rusm9snik2013@yandex.ru>
2020-12-04 21:20:51 +0300

    dispatch and action

--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom';
 import store from './redux/state';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {addPost, updateNewPostText} from "./redux/state";
-import {BrowserRouter} from "react-router-dom";
+
 
 
 let rerenderEntireTree = (state) => {
     ReactDOM.render(
-            <App state={state} addPost={store.addPost.bind(store)}
-                 updateNewPostText={store.updateNewPostText.bind(store)}/>
+            <App state={state} dispatch={store.dispatch.bind(store)}/>
         , document.getElementById('root')
     );
 }
--- src/redux/state.js
+++ src/redux/state.js
@@ -25,31 +25,34 @@ let store = {
         },
         sidebar: {}
     },
-    getState() {
-        return this._state;
-    },
     _callSubscriber() {
         console.log('state changed')
     },
-    addPost () {
 
-        let newPost = {
-            id: 5,
-            message: this._state.profilePage.newPostText,
-            likesCount: 0
-        }
-
-        this._state.profilePage.posts.unshift(newPost)
-        this._state.profilePage.newPostText = ''
-        this._callSubscriber(this._state)
-    },
-    updateNewPostText(newText){
-        this._state.profilePage.newPostText = newText
-        this._callSubscriber(this._state)
+    getState() {
+        return this._state;
     },
     subscribe(observer) {
         this._callSubscriber = observer; // наблюдатель
     },
+
+
+    dispatch (action) { // { type: 'ADDPOST' }
+        if (action.type === 'ADD-POST') {
+            let newPost = {
+                id: 5,
+                message: this._state.profilePage.newPostText,
+                likesCount: 0
+            }
+
+            this._state.profilePage.posts.unshift(newPost)
+            this._state.profilePage.newPostText = ''
+            this._callSubscriber(this._state)
+        } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+            this._state.profilePage.newPostText = action.newText
+            this._callSubscriber(this._state)
+        }
+    }
 }
 
 

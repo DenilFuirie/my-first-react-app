c5f326212f4384616226f977c7b1052c7029c07c
Daniil <rusm9snik2013@yandex.ru>
2020-12-14 00:03:06 +0300

    Added containers component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import {BrowserRouter, Route} from "react-router-dom";
+import DialogsContainer from "./components/Dialogs/DialogsContainer";
 
 
 const App = (props) => {
@@ -20,13 +21,12 @@ const App = (props) => {
             <Nav/>
             <div className="app-wrapper-content">
                 <Route path='/dialogs'
-                       render={() => <Dialogs
+                       render={() => <DialogsContainer
                            store={props.store}
                        />}/>
                 <Route path='/profile'
                        render={() => <Profile
-                           profilePage={props.state.profilePage}
-                           dispatch={props.dispatch}
+                           store={props.store}
                        />}/>
 
                 <Route path='/news'
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -7,20 +7,19 @@ import {sendMessageCreator, updateNewMessageBodyCreator} from "../../redux/dialo
 
 const Dialogs = (props) => {
 
-    let state = props.store.getState().dialogsPage
+    let state = props.dialogsPage
 
     let dialogElements = state.dialogs.map((d) => <DialogItem name={d.name} id={d.id} /> );
     let messagesElement = state.messages.map((m) => <Message message={m.message} id={m.id}/>)
     let newMessageBody  = state.newMessageBody
 
-    let newMessageElement = React.createRef()
 
     let onSendMessageClick = () => {
-        props.store.dispatch(sendMessageCreator())
+        props.sendMessage()
     }
     let onNewMessageChange = (e) => {
         let body = e.target.value
-        props.store.dispatch(updateNewMessageBodyCreator(body))
+        props.updateNewMessageBody(body)
     }
 
     return (

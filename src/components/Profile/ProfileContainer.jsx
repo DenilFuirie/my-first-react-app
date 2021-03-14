f34e3b586f8015c53f90ce0ca6721a675ba00f00
Daniil <rusm9snik2013@yandex.ru>
2021-03-14 16:04:02 +0300

    add redirect if user not authorized

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,7 +3,7 @@ import Profile from "./Profile";
 import * as axios from "axios";
 import {connect} from "react-redux";
 import {getUserProfile, setUserProfile} from "../../redux/profile-reducer";
-import {withRouter} from "react-router-dom";
+import {Redirect, withRouter} from "react-router-dom";
 import {usersAPI} from '../../api/api';
 
 class ProfileContainer extends React.Component {
@@ -14,10 +14,14 @@ class ProfileContainer extends React.Component {
         }
 
         this.props.getUserProfile(userId);
+
+
     }
 
     render() {
-
+        if(!this.props.isAuth) {
+            return <Redirect to="/Login" />
+        }
         return (
             <Profile {...this.props} profile={this.props.profile}/>
         )
@@ -25,7 +29,8 @@ class ProfileContainer extends React.Component {
 }
 
 let mapStateToProps = (state) => ({
-    profile: state.profilePage.profile
+    profile: state.profilePage.profile,
+    isAuth: state.auth.isAuth
 })
 
 let withUrlDataContainerComponent = withRouter(ProfileContainer);
